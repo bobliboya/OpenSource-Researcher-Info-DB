c954c007d7bfafdda219e089d7c5d9d2d9e3b33f
Boyang Li <58096549+bobliboya@users.noreply.github.com>
2024-11-14 15:54:52 -0600

    Add the data source used to create tables and the table creation SQL instruction

---
+++ doc/datasource_and_table_create/table_create/topic_create.sql
@@ -0,0 +1,6 @@
+create table topic (
+    topic_id varchar(255),
+	topic_name varchar(255),
+    category varchar(255),
+    primary key (topic_id)
+);
---
+++ doc/datasource_and_table_create/table_create/university_create.sql
@@ -0,0 +1,7 @@
+create table university (
+	InstitutionId int,
+    Institution_Name varchar(255),
+    City varchar(255),
+    Country varchar(255),
+    primary key (InstitutionId)
+);
