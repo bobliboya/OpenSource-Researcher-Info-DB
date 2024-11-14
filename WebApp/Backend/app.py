f415f6d46d21fa4e15be6571d29e903cb3985cc3
Philip James <philipjames2004@gmail.com>
2024-11-14 16:47:57 -0600

    added info on how to connect to GCP database

--- WebApp/Backend/app.py
+++ WebApp/Backend/app.py
@@ -1,11 +1,39 @@
 # app.py
 from flask import Flask
+import mysql.connector
+from s import *
+
 
 app = Flask(__name__)
+db_config = {
+    'user': db_user,
+    'password': db_password,
+    'host': db_connection_name,  # Use the Public IP address, not the instance connection name
+    'database': db_name
+}
+
+
+
+# Initialize the connection
+def get_db_connection():
+    print("Connecting...")
+    conn = mysql.connector.connect(**db_config)
+    print("Connected!")
+    return conn
+
+@app.route("/authors")
+def get_data():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM author")
+    data = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return {"data": data}
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return db_config['user']
 
 if __name__ == "__main__":
     app.run(debug=True)
