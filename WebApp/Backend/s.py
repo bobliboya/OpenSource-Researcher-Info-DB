581f3287f632be2b11231e86e7d48a99e9c35331
alexyy2004 <116599984+alexyy2004@users.noreply.github.com>
2024-11-17 13:35:32 -0600

    Delete WebApp/Backend/s.py

--- WebApp/Backend/s.py
+++
@@ -1,6 +0,0 @@
-db_user = 'FlaskPJ'
-db_password = 'JxM_m`N}uHt-@rX-'
-db_name = 'opensource'
-db_connection_name = '34.56.153.64' #'opensource-439720:us-central1:db-opensource-fa24'
-
-# Your MySQL connection id is 394028
