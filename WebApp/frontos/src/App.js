8760a4b9d5b363ca90946b45c200b39080976174
Chinmay Bhogilal <chinmay9@illinois.edu>
2024-11-17 15:57:18 -0600

    Table format for authors

--- WebApp/frontos/src/App.js
+++ WebApp/frontos/src/App.js
@@ -14,30 +14,35 @@ function App() {
   }, []);
 
   return (
-    <div>
-        <h1>Authors:</h1>
-        {authors.length > 0 ? (
-            <ul>
-                {(() => {
-                    const items = [];
-                    for (let i = 0; i < authors.length; i++) {
-                        items.push(
-                            <li key={i}>
-                                {authors[i].name} 
-                                  (
-                                    ID: {authors[i].author_id}
-                                    Name: {authors[i].author_name}
-                                    Uni Name: {authors[i].university_name}
-                                  )
-                            </li>
-                        );
-                    }
-                    return items;
-                })()}
-            </ul>
-        ) : (
-            <p>Loading...</p>
-        )}
+    <div className="container">
+        <hi className="page-header text-center">Group 096 Opensource Project</hi>
+        <hi className="page-header text-center">Below is a table of authors.</hi>
+        <table border="1" cellPadding="10" style={{ borderCollapse: "collapse", width: "100%" }}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>UniName</th>
+          </tr>
+        </thead>
+        <tbody>
+          {authors.length > 0 ? (
+            authors.map((row) => (
+              <tr key={row.id}>
+                <td>{row.author_id}</td>
+                <td>{row.author_name}</td>
+                <td>{row.university_name}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan="3" style={{ textAlign: "center" }}>
+                Loading
+              </td>
+            </tr>
+          )}
+        </tbody>
+        </table>
     </div>
 );
 }
